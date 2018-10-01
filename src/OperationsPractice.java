bccb885911ea970d18da0d1ec3bc77d9b217f989
dhedrick21 <43002631+dhedrick21@users.noreply.github.com>
2018-10-01 08:32:13 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,24 @@
+
+public class OperationsPractice 
+{
+
+	public static void main(String[] args)
+	{
+		
+	int number;
+	number = 20;
+	System.out.println("Number: " + number);
+	number = number + 5;
+	System.out.println("Number + 5: " + number);
+	number /= 2;
+	System.out.println("Number / 2: " + number);
+	number *= 4;
+	System.out.println("Number * 4: " + number);
+	number -= 6;
+	System.out.println("Number - 6: " + number);
+	number %= 2;
+	System.out.println("Number % 2: " + number);
+	
+	}
+
+}
